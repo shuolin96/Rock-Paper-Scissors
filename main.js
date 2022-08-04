4fc9d465ff2df16c0105c0abaade9f1fcf20341c
Shuolin Hu <shuolin96@gmail.com>
2022-08-04 00:46:31 -0400

    Add UI

--- main.js
+++ main.js
@@ -1,73 +1,57 @@
-function getComputerChoice() {
-    let choice;
-    let random_number = Math.floor(Math.random() * 3);
-    if (random_number === 0) {
-        choice = 'Rock';
-    } else if (random_number === 1) {
-        choice = 'Paper';
-    } else {
-        choice = 'Scissors';
-    }
-    console.log(choice)
-    return choice;
-}
- 
-
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === 'rock') {
-        if (computerSelection.toLowerCase() === 'rock') {
+function getComputerChoice() {
+    let choice = ['rock', 'paper', 'scissors'];
+    let random_number = Math.floor(Math.random() * 3);
+    return choice[random_number]
+}
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'rock') {
             return "No Winner!"
-        } else if (computerSelection.toLowerCase() === 'scissors') {
+        } else if (computerSelection === 'scissors') {
             playerScore ++;
             return "You Win! Rock beats Scissors"
         } else {
             computerScore ++;
             return "You Lose! Paper beats Rock"
         }
-    } else if (playerSelection.toLowerCase() === 'paper') {
-        if (computerSelection.toLowerCase() === 'paper') {
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'paper') {
             return "No Winner!"
-        } else if (computerSelection.toLowerCase() === 'rock') {
+        } else if (computerSelection === 'rock') {
             playerScore ++;
             return "You Win! Paper beats Rock"
         } else {
             computerScore ++;
             return "You Lose! Scissors beats Paper"
         }
-    } else if (playerSelection.toLowerCase() === 'scissors') {
-        if (computerSelection.toLowerCase() === 'scissors') {
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'scissors') {
             return "No Winner!"
-        } else if (computerSelection.toLowerCase() === 'paper') {
+        } else if (computerSelection === 'paper') {
             playerScore ++;
             return "You Win! Scissors beats Paper"
         } else {
             computerScore ++;
             return "You Lose! Rock beats Scissors"
         }       
-    } else {
-        return "Please Input Rock, Paper or Scissors!"
-    }
+    } 
 }
 
-function game() {
+let buttons = document.querySelectorAll('button');
+let board = document.querySelector('.board');
+let score = document.querySelector('.score');
 
-    for (let i = 0; i < 5; i++) {
-
-        let playerSelection = prompt("Please input your choice: ");
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    if (playerScore > computerScore) {
-        console.log(`Your Score: ${playerScore} {\n} Computer Score: ${computerScore}`);
-        console.log("You Win!");
-    } else {
-        console.log(`Your Score: ${playerScore} {\n} Computer Score: ${computerScore}`);
-        console.log("You Lose!") ;
-    }
-}
-
-game()
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        board.textContent = playRound(this.value);
+        score.textContent = `Your Score: ${playerScore} Computer Score: ${computerScore}`;
+        if (playerScore === 5) alert("You win!");
+        if (computerScore === 5) alert("You lost!")
+    });
+})
 
